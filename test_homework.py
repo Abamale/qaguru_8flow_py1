2ec70ba20d2f0f7fdc104fd9dd5f72f7746974db
marabakumova <abakumova.ma.le@gmail.com>
2023-10-11 14:21:15 +0300

    Изменила вывод значений словаря

--- test_homework.py
+++ test_homework.py
@@ -86,7 +86,7 @@ def test_dicts():
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
     d = dict(zip(first, second))
-    print(d)
+    print(d.values())
 
     assert isinstance(d, dict)
     assert len(d) == 5
